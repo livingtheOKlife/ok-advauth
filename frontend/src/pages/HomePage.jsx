fdc25bbdf7079dadeefab7e5ddeb5c5fb80c3ba8
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-04-16 14:33:03 +0100

    frontend - initial pages
    HomePage
    AboutPage
    PageNotFoundPage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom'
+
 import MainContainer from '../components/layout/MainContainer'
 
 function HomePage () {
   return (
     <MainContainer page='home-page'>
-      MainContainer - Home Page
+      <h2><span className="logo"><em>OK</em></span> advanced authentication</h2>
+      <p>This is a boilerplate for MERN authentication that stores a JWT in an HTTP-ONly cookie. It also uses Redux Toolkit and my own SASS library.</p>
+      <p>Includes email verification and form validation.</p>
+      <div className="buttons">
+        <Link to='/login' className={`btn md`} >Sign in</Link>
+        <Link to='/register' className={`btn md`} >Sign up</Link>
+      </div>
     </MainContainer>
   )
 }
--- frontend/src/pages/PageNotFoundPage.jsx
+++ frontend/src/pages/PageNotFoundPage.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 import MainContainer from '../components/layout/MainContainer'
 
 function PageNotFoundPage () {
   return (
     <MainContainer page='page-not-found-page'>
-      MainContainer - Page Not Found Page
+      <h3>Whoops!</h3>
+      <span>404 Error: Page not found.</span>
+      <Link to='/'>Return home</Link>
     </MainContainer>
   )
 }
