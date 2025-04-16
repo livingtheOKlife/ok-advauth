b5bd9a0c9622bf11fc9e2805442e7649667188b3
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-04-16 13:49:54 +0100

    backend - initial set up
    error middleware
    database configuration
    email configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import connectDB from './config/database.config.js'
+
+import { notFound, errorHandler } from './middleware/error.middleware.js'
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
@@ -9,4 +13,10 @@ const app = express()
 
 app.get('/', () => console.log('Server is ready'))
 
-app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  connectDB()
+  console.log(`Server is listening on port: ${PORT}`)
+})
